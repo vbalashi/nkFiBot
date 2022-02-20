b4ab7fea8d6281e797c2db4f4ff6be742cb2dcdf
vbalashi <vbalashi@gmail.com>
2022-02-20 10:27:03 +0300

    Delete bot.ts

--- bot.ts
+++
@@ -1,8 +0,0 @@
-import { Bot } from "https://deno.land/x/grammy/mod.ts";
-
-const bot = new Bot("1917307536:AAEhCsfHSVjl2FLKXtTmMEG_UwmVaUhCUkk");
-
-bot.command("start", (ctx) => ctx.reply("welcome"));
-bot.on("message", (ctx) => ctx.reply("got a message"));
-
-bot.start();
